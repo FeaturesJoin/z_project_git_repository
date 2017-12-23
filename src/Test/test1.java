43823a5de17ead0a782ef3b12c8bf0e0f05ca281
Zi <lll0001102@163.com>
2017-12-23 14:25:06 +0800

    second commit

---
+++ src/Test/test1.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class test1 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = a+1;
+        System.out.print(".qq....");
+        System.out.print("b...");
+        System.out.print(".....");
+    }
+}
